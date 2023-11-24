e2cc47823c82b90122ae8ad745ce36d3c5b9b0db
stepholo <oloobrian89@gmail.com>
2023-11-24 15:43:03 +0300

    parent class

--- tasks/base_task.py
+++ tasks/base_task.py
@@ -4,16 +4,20 @@
 """
 
 
-import uuid
+import tasks
 from datetime import datetime
-from tasks import storage
+from sqlalchemy import Column, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+Base = declarative_base()
 
 
 class TaskManager:
     """Parent class TaskManager that defines attributes and methods
         to be inherited by the child classes.
         Attributes and methods:
-            id: task instance assigned by random uuid (uuid4())
             created_at: the current date and time when the instance was created
             updated_at: the current date and time when the instance was updated
             __str__: String representation of the instance
@@ -21,20 +25,15 @@ class TaskManager:
             to_dict: method to return keys/values of __dict__ of the task
             instance.
     """
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """class construct"""
 
         if args:
             raise TypeError('Positional arguments is not accepted(*args)')
-        mandatory_args = {'user_name', 'user_email'}
-        missing_args = mandatory_args - set(kwargs.keys())
 
-        if missing_args:
-            raise ValueError(
-                f"Missing mandatory arguments(s): {', '.join(missing_args)}")
-        self.id = kwargs.get('id', str(uuid.uuid4()))
-        self.user_name = kwargs['user_name']
-        self.user_email = kwargs['user_email']
         self.created_at = kwargs.get('created_at', datetime.now())
         self.updated_at = kwargs.get('updated_at', datetime.now())
 
@@ -43,22 +42,27 @@ class TaskManager:
             the format of [<class name>] (<self.id>) <self.__dict__>
         """
         obj_name = self.__class__.__name__
-        obj_id = self.id
-        return f"[{obj_name}] ({obj_id}) {self.__dict__}"
+        return f"[{obj_name}] {self.__dict__}"
 
     def save(self):
         """Method to update the attribute updated_at with
             the current datetime
         """
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        tasks.storage.new(self)
+        tasks.storage.save()
 
     def to_dict(self):
         """Method to return keys/value of __dict__ of the class instance
         """
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = obj_dict["created_at"].isoformat()
-        obj_dict['updated_at'] = obj_dict["updated_at"].isoformat()
+        if 'created_at' in obj_dict:
+            obj_dict['created_at'] = obj_dict['created_at'].strftime(time)
+        if 'updated_at' in obj_dict:
+            obj_dict['updated_at'] = obj_dict['updated_at'].strftime(time)
+        obj_dict["__class__"] = self.__class__.__name__
         return obj_dict
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        tasks.storage.delete(self)
